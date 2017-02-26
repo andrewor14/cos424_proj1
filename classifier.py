0634e816ddcbabcfa2b2533e15ecac01a3fc40f2
Andrew Or <andrewor14@gmail.com>
2017-02-26 15:57:08 -0500

    Add scikit-learn BNB

--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@
 from preprocessSentences import clean_word, clean_words, parse_example
 
 import argparse
+import numpy as np
 import sys
 
 def main():
@@ -11,12 +12,84 @@ def main():
   parser.add_argument("-b", "--bow", help="bag of words file", required=True)
   parser.add_argument("-v", "--vocab", help="vocab file", required=True)
   parser.add_argument("-e", "--test", help="test data file", required=True)
+  parser.add_argument("-m", "--model", help="model", required=True)
   args = parser.parse_args()
 
+  # Build vocabs list from train dataset
+  vocabs = []
+  with open(args.vocab, "r") as f:
+    for line in f.readlines():
+      vocabs += [clean_word(line.decode("utf-8-sig"))]
+
+  # Do the classifying
+  if args.model == 'bnb':
+    bernoulli_naive_bayes(args.train, args.bow, vocabs, args.test)
+  elif args.model == 'mnb':
+    multinomial_naive_bayes(args.train, args.bow, vocabs, args.test)
+  elif args.model == 'skbnb':
+    sk_bernoulli_naive_bayes(args.train, args.bow, vocabs, args.test)
+  else:
+    raise Exception("Unknown model '%s' % args.model")
+
+def sk_bernoulli_naive_bayes(train_file, bow_file, vocabs, test_file):
+  train_labels = []
+  bow_data = []
+  with open(train_file, "r") as f:
+    for line in f.readlines():
+      train_labels += [int(line.split()[-1])]
+  with open(bow_file, "r") as f:
+    for line in f.readlines():
+      bow_data += [[int(v) for v in line.split(",")]]
+  train_labels = np.array(train_labels)
+  bow_data = np.array(bow_data)
+  from sklearn.naive_bayes import BernoulliNB
+  clf = BernoulliNB()
+  clf.fit(bow_data, train_labels)
+  # Test it!
+  vocab_index = {}
+  log_threshold = 10
+  num_test_examples = 0
+  num_test_correct = 0
+  for i, word in enumerate(vocabs):
+    vocab_index[word] = i
+  with open(test_file, "r") as f:
+    for i, line in enumerate(f.readlines()):
+      expected_label = int(line.split()[-1])
+      # Turn sentences into bag of word vectors
+      word_vector = [0] * len(vocabs)
+      words = clean_words(parse_example(line).split())
+      for word in words:
+        index = vocab_index.get(word)
+        if index > 0:
+          word_vector[index] += 1
+      word_vector = np.array(word_vector)
+      # Do the prediction
+      predicted_label = clf.predict([word_vector])
+      if predicted_label == expected_label:
+        num_test_correct += 1
+      num_test_examples += 1
+      if i < log_threshold:
+        print "------------------------------------------------------------------------"
+        print "Classifying line:"
+        print "  %s" % line.strip()
+        print "  words = [%s]" % (", ".join(words))
+        print "  predicted label = %s, expected label = %s" % (predicted_label, expected_label)
+
+  print "\n================================"
+  print "You guessed %s/%s correct." % (num_test_correct, num_test_examples)
+  print "================================\n"
+
+def bernoulli_naive_bayes(train_file, bow_file, vocabs, test_file):
+  manual_naive_bayes(train_file, bow_file, vocabs, test_file, bernoulli=True)
+
+def multinomial_naive_bayes(train_file, bow_file, vocabs, test_file):
+  manual_naive_bayes(train_file, bow_file, vocabs, test_file, bernoulli=False)
+
+def manual_naive_bayes(train_file, bow_file, vocabs, test_file, bernoulli):
   # Calculate prior
   train_labels = []
-  with open(args.train, "r") as train_data:
-    for line in train_data.readlines():
+  with open(train_file, "r") as f:
+    for line in f.readlines():
       train_labels += [int(line.split()[-1])]
   num_train_examples = len(train_labels)
   num_train_positive = sum([l for l in train_labels if l == 1])
@@ -26,17 +99,10 @@ def main():
   print "Num train examples = %s, positive prior = %s, negative prior = %s" %\
     (num_train_examples, positive_prior, negative_prior)
 
-  # Build vocab dictionary
-  vocabs = []
-  with open(args.vocab, "r") as vocab_data:
-    for line in vocab_data.readlines():
-      vocabs += [clean_word(line.decode("utf-8-sig"))]
-  num_features = len(vocabs)
-
   # Build up document frequency
   total_documents_by_word = {}
-  with open(args.bow, "r") as bow_data:
-    for i, line in enumerate(bow_data.readlines()):
+  with open(bow_file, "r") as f:
+    for i, line in enumerate(f.readlines()):
       label = train_labels[i]
       vector = [int(v) for v in line.split(",")]
       assert len(vector) == len(vocabs),\
@@ -49,8 +115,8 @@ def main():
   # Build up word counts by label, taking into account TF * IDF
   pos_counts_by_word = {}
   neg_counts_by_word = {}
-  with open(args.bow, "r") as bow_data:
-    for i, line in enumerate(bow_data.readlines()):
+  with open(bow_file, "r") as f:
+    for i, line in enumerate(f.readlines()):
       vector = [int(v) for v in line.split(",")]
       label = train_labels[i]
       counts_by_word = pos_counts_by_word if label == 1 else neg_counts_by_word
@@ -59,9 +125,12 @@ def main():
         if value > 0 and word in vocabs:
           if word not in counts_by_word:
             counts_by_word[word] = 0
-          tf = value
-          idf = float(num_train_examples) / total_documents_by_word[word]
-          counts_by_word[word] += tf * idf
+          if bernoulli:
+            counts_by_word[word] += 1
+          else:
+            tf = value
+            idf = float(num_train_examples) / total_documents_by_word[word]
+            counts_by_word[word] += tf * idf
   print "Using %s features: %s..." % (num_features, ", ".join(list(vocabs)[:10]))
   print "%s features are used in positive reviews: %s..." %\
     (len(pos_counts_by_word), ", ".join(pos_counts_by_word.keys()[:10]))
@@ -73,14 +142,14 @@ def main():
     counts_by_word = pos_counts_by_word if label == 1 else neg_counts_by_word
     num_train_examples = num_train_positive if label == 1 else num_train_negative
     word_count = counts_by_word[word] if word in counts_by_word else 0
-    return float(word_count + 1) / (num_train_examples + num_features)
+    return float(word_count + 1) / (num_train_examples + (2 if bernoulli else num_features))
 
   # Do some classifying!
   num_test_correct = 0
   num_test_examples = 0
-  log_threshold = 10
-  with open(args.test, "r") as test_data:
-    for i, line in enumerate(test_data.readlines()):
+  log_threshold = 0
+  with open(test_file, "r") as f:
+    for i, line in enumerate(f.readlines()):
       words = clean_words(parse_example(line).split())
       expected_label = int(line.split()[-1])
       pos_probability = positive_prior
@@ -97,7 +166,6 @@ def main():
         print "Classifying line:"
         print "  %s" % line.strip()
         print "  words = [%s]" % (", ".join(words))
-        print "  label = %s" % label
         print "  positive probability = %s, negative probability = %s" % (pos_probability, neg_probability)
         print "  predicted label = %s, expected label = %s" % (predicted_label, expected_label)
 
